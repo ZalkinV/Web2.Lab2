e43623c74d4b8aa47b7630ef8e720037507fe83d
Zalkin <Zalkin.viktor@mail.ru>
2019-10-20 18:24:33 +0300

    Move learning redux to index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Geolocation from "./components/Geolocation"
 import AddFavorite from "./components/AddFavorite"
-import { createStore } from "redux";
 
 class App extends React.Component {
   render() {
@@ -14,41 +13,6 @@ class App extends React.Component {
     );
   }
 }
-
-const initialState = {
-  city: {
-    name: undefined,
-    temp: undefined,
-    wind: undefined,
-    humidity: undefined,
-    pressure: undefined,
-    coords: undefined
-  },
-  favorites: []
-};
-
-function reducer(state = initialState, action) {
-  state = {
-    ...state,
-    city: action.payload
-  };
-  return state;
-}
-
-const store = createStore(reducer);
-store.subscribe(() => {
-  console.log("Store was changed!", store.getState());
-});
-
-store.dispatch({
-  type: 1,
-  payload: 1
-});
-
-store.dispatch({
-  type: 1,
-  payload: 2
-});
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,45 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from "redux";
+
+
+const initialState = {
+  weather: {
+    cityName: undefined,
+    temp: undefined,
+    wind: undefined,
+    humidity: undefined,
+    pressure: undefined,
+    coords: undefined
+  },
+  favorites: []
+};
+
+function reducer(state = initialState, action) {
+  state = {
+    ...state,
+    city: action.payload,
+  };
+  return state;
+}
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log("Store was changed!", store.getState());
+});
+
+store.dispatch({
+  type: 1,
+  payload: 1
+});
+
+store.dispatch({
+  type: 1,
+  payload: 2
+});
+
+
 ReactDOM.render(
   <App name="Viktor" />,
   document.getElementById('root')
