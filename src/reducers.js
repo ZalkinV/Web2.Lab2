2d40fcff010d6440f41bdc7b2104a2018f38322f
Zalkin <Zalkin.viktor@mail.ru>
2019-10-20 22:43:11 +0300

    reducer. Add using enum Actions

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,29 @@
+import { Actions } from "./actions";
+
+
 function reducer(state, action) {
-  console.log("reducer", action);
+  console.log("Reducer", action);
   state = {
     ...state,
     weather: {
       ...state.weather,
-      city: action.payload
-    }
+    },
+    favorites: [...state.favorites]
   };
+
+  switch (action.type) {
+    case Actions.ADD_FAVORITE:
+      break;
+    
+    case Actions.DELETE_FAVORITE:
+      break;
+
+    default:
+      break;
+  }
   
   return state;
 }
+
 
 export default reducer;
