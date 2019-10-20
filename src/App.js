e4096c5b1101dec7870b0ae425cfefef7c6d560f
Zalkin <Zalkin.viktor@mail.ru>
2019-10-20 17:47:44 +0300

    Start using simple redux with mutable state in reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Geolocation from "./components/Geolocation"
 import AddFavorite from "./components/AddFavorite"
+import { createStore } from "redux";
 
 class App extends React.Component {
   render() {
@@ -13,5 +14,38 @@ class App extends React.Component {
     );
   }
 }
+
+const initialState = {
+  city: {
+    name: undefined,
+    temp: undefined,
+    wind: undefined,
+    humidity: undefined,
+    pressure: undefined,
+    coords: undefined
+  },
+  favorites: []
+};
+
+function reducer(state = initialState, action) {
+  state.city = action.payload;
+  return state;
+}
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log("Store was changed!", store.getState());
+});
+
+store.dispatch({
+  type: 1,
+  payload: 1
+});
+
+store.dispatch({
+  type: 1,
+  payload: 2
+});
+
 
 export default App;
