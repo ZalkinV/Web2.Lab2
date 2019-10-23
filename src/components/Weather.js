cfec61decd616a2484f0d5aea6481774c48476de
Zalkin <Zalkin.viktor@mail.ru>
2019-10-24 02:05:26 +0300

    middlewares. Add getIconURL() and its using in Weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { getIconURL } from "../middlewares";
 
 
 export default function Weather(props) {
   return (
     <div>
       <div>{props.forecast.cityName}</div>
+      <img src={getIconURL(props.forecast.icon)} alt="Weather icon" />
     </div>
   );
 }
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,7 @@
 const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_BASE_PARAMETERS = "&appid=7825ce4ffa896c5019e53087c858568a&units=metric&lang=en"
+const API_ICON_URL = "https://openweathermap.org/img/wn/";
+
 
 export function fetchWeatherByCityName(cityName) {
   const API_URL = `${API_BASE_URL}?q=${cityName}${API_BASE_PARAMETERS}`;
@@ -9,6 +11,10 @@ export function fetchWeatherByCityName(cityName) {
 export function fetchWeatherByCoords(coords) {
   const API_URL = `${API_BASE_URL}?lat=${coords.lat}&lon=${coords.lon}${API_BASE_PARAMETERS}`;
   return fetchWeather(API_URL);
+}
+
+export function getIconURL(iconCode) {
+  return `${API_ICON_URL}${iconCode}.png`;
 }
 
 function fetchWeather(url) {
