55690298df453e0cae7d97ed1461bed3b8317135
Zalkin <Zalkin.viktor@mail.ru>
2019-10-23 12:03:13 +0300

    middlewares. Add fetchWeatherByCoords() and general fetchWeather()

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,11 +5,20 @@ const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_BASE_PARAMETERS = "&appid=7825ce4ffa896c5019e53087c858568a&units=metric&lang=en"
 
 export function fetchWeatherByCityName(cityName) {
-  return dispatch => {
+  const API_URL = `${API_BASE_URL}?q=${cityName}${API_BASE_PARAMETERS}`;
+  return fetchWeather(API_URL);
+}
+
+export function fetchWeatherByCoords(coords) {
+  const API_URL = `${API_BASE_URL}?lat=${coords.lat}&lon=${coords.lon}${API_BASE_PARAMETERS}`;
+  return fetchWeather(API_URL);
+}
+
+function fetchWeather(url) {
+  return function(dispatch) {
     dispatch(fetchWeatherPending());
 
-    const API_URL = `${API_BASE_URL}?q=${cityName}${API_BASE_PARAMETERS}`;
-    fetch(API_URL)
+    fetch(url)
       .then(response => {
         response.json().then(data => {
           dispatch(fetchWeatherSuccess(data));
@@ -19,5 +28,5 @@ export function fetchWeatherByCityName(cityName) {
       .catch(error => {
         dispatch(fetchWeatherError(error))
       });
-  };
+  }
 }
