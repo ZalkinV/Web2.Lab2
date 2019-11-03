0c655dc8a23f18de55ac271037e642d14649ad0b
Zalkin <Zalkin.viktor@mail.ru>
2019-11-03 22:39:22 +0300

    Updates

---
+++ precache-manifest.cdf5a75531963b5bd29cdbb93677d724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4316b775d2664285e437536b0f40246",
+    "url": "/Web2.Lab2/index.html"
+  },
+  {
+    "revision": "991578f861f562bc2690",
+    "url": "/Web2.Lab2/static/css/main.4898a355.chunk.css"
+  },
+  {
+    "revision": "6903cac9082de358d1fd",
+    "url": "/Web2.Lab2/static/js/2.44cab1f3.chunk.js"
+  },
+  {
+    "revision": "991578f861f562bc2690",
+    "url": "/Web2.Lab2/static/js/main.c659f596.chunk.js"
+  },
+  {
+    "revision": "b8e532904960062ce92f",
+    "url": "/Web2.Lab2/static/js/runtime-main.3c4325c3.js"
+  }
+]);
