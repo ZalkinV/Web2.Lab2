94e5d6c5faa358efe61f0eb86bbe61328321712d
Zalkin <Zalkin.viktor@mail.ru>
2019-11-12 23:03:28 +0300

    Loader. Remove unused props from input

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./Loader.css";
 
 
-export default function Loader(props) {
+export default function Loader() {
   return (
     <div className="loader">
       <img src={"/loader.svg"} alt="loader" />
