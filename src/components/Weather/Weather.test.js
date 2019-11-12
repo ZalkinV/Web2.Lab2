08cbebacae3fb4295d2ca16d8a0a16d43cc8f32b
Zalkin <Zalkin.viktor@mail.ru>
2019-11-12 22:50:45 +0300

    Weather. Add snapshot test 'should has forecast and delete button'

---
+++ src/components/Weather/Weather.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Weather from "./Weather";
+
+
+describe("<Weather /> render", () => {
+
+  const DEFAULT_FORECAST = {
+    cityName: "Moscow",
+    temperature: "-4",
+    icon: "icon_code",
+    windSpeed: 10,
+    description: "nice to swim",
+    pressure: 234,
+    humidity: 99,
+    coords: {
+      lat: 40,
+      lon: 40
+    }
+  };
+
+  test("should has forecast and delete button", () => {
+    const tree = renderer
+      .create(
+        <Weather
+          onFetch={() => {}}
+          onDelete={() => {}}
+          forecast={DEFAULT_FORECAST}
+        />)
+      .toJSON();
+    
+    expect(tree).toMatchSnapshot();
+  });
+
+});
