5f14f148b6afd0b749cbe4c27bbd3b88bc4d43a3
Zalkin <Zalkin.viktor@mail.ru>
2019-11-20 10:16:44 +0300

    Favorites.test. Add test 'should has favorites with 1 element'

--- src/components/Favorites/Favorites.test.js
+++ src/components/Favorites/Favorites.test.js
@@ -9,6 +9,20 @@ import Favorites from "./Favorites";
 
 describe("<Favorites /> render", () => {
 
+  const DEFAULT_FORECAST = {
+    cityName: "New York",
+    temperature: "-22",
+    icon: "icon_code",
+    windSpeed: 15,
+    description: "good to stay at home",
+    pressure: 234,
+    humidity: 1,
+    coords: {
+      lat: 74,
+      lon: 89
+    }
+  };
+
   const storeCreator = configureStore([thunk]);
 
   beforeAll(() => {
@@ -45,6 +59,24 @@ describe("<Favorites /> render", () => {
     const store = storeCreator({
       fav: {
         favorites: new Map()
+      }
+    });
+
+    const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Favorites />
+      </Provider>
+    )
+    .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("should has favorites with 1 element", () => {
+    const store = storeCreator({
+      fav: {
+        favorites: new Map([["firstCity", DEFAULT_FORECAST]])
       }
     });
 
