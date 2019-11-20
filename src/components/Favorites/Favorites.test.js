3b8cf57b0d868057fc4654d65de9ddefc8f5a858
Zalkin <Zalkin.viktor@mail.ru>
2019-11-20 10:05:21 +0300

    Favorites. Create first test 'should has error'

---
+++ src/components/Favorites/Favorites.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import thunk from "redux-thunk";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+import Favorites from "./Favorites";
+
+
+describe("<Favorites /> render", () => {
+
+  const storeCreator = configureStore([thunk]);
+
+  beforeAll(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue("fake success response")
+    });
+  });
+
+  afterAll(() => {
+    delete global.fetch;
+  });
+
+  test("should has error", () => {
+    const store = storeCreator({
+      fav: {
+        error: "test error message",
+        favorites: new Map()
+      }
+    });
+
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Favorites />
+        </Provider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+});
